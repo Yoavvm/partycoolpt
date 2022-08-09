b80b51ba335f2233f97c853fe0c76a49e6091e68
Toavvm <yoav.vm@gmail.com>
2022-08-09 12:20:59 +0300

    fixed date formatting

--- partycoolclient/src/app/components/book-event-form/book-event-form.component.ts
+++ partycoolclient/src/app/components/book-event-form/book-event-form.component.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { LanguageService } from 'src/app/services/language.service';
 
@@ -14,7 +15,16 @@ export class BookEventFormComponent implements OnInit {
 
   public languageShort = this.languageService.languageConfig[this.languageService.selectedLanguage];
 
-  public currentDate = Date.now();
+  public formatDate = (): string => {
+    let year: string = new Date().getFullYear().toString();
+    let month: number | string = new Date().getMonth() + 1;
+    let day: number | string = new Date().getDate();
+
+    month >= 10 ? month.toString() : month = "0" + month.toString();
+    day >= 10 ? day.toString() : day = "0" + day.toString();
+
+    return year + "-" + month + "-" + day;
+  }
 
   ngOnInit(): void {
   }
